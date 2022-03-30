f3d1b1c334a72bbbafacc877185b8cde580b1e01
GTech9971 <98648140+GTech9971@users.noreply.github.com>
2022-03-30 23:18:20 +0900

    登録画面の項目追加

--- UdonMaestro-App/src/app/presentations/registry-page/registry.page.ts
+++ UdonMaestro-App/src/app/presentations/registry-page/registry.page.ts
@@ -12,6 +12,19 @@ export class RegistryPage {
 
     readonly WEEKS: string[] = ['月', '火', '水', '木', '金', '土', '日', '祝'];
 
+
+    startTime: string;
+    endTime: string;
+
     constructor() {
+    }
+
+
+    formatDate(dateStr: string): string {
+        let date: Date = new Date(dateStr);
+        const HOURS = ("00" + (date.getHours())).slice(-2)
+        const MINS = ("00" + (date.getMinutes())).slice(-2)
+
+        return `${HOURS}:${MINS}`;
     }
 }
